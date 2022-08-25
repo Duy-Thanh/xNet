251610aac8669dc64ecc09749ce0bef9e2f5913c
Duy-Thanh <36120800+Duy-Thanh@users.noreply.github.com>
2022-08-25 09:16:19 +0700

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,18 +7,18 @@ using System.Resources;
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
 [assembly: AssemblyTitle("xNet")]
-[assembly: AssemblyDescription("https://github.com/X-rus/xNet")]
+[assembly: AssemblyDescription("https://github.com/Duy-Thanh/xNet")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ruslan Khuduev")]
+[assembly: AssemblyCompany("Ruslan Khuduev, CyberDay Studio")]
 [assembly: AssemblyProduct("xNet")]
-[assembly: AssemblyCopyright("Copyright © 2012-2016 Ruslan Khuduev <x-rus@list.ru>")]
+[assembly: AssemblyCopyright("Copyright © 2012-2016 Ruslan Khuduev <x-rus@list.ru>, Copyright © 2022 - present CyberDay Studio. Modified by Duy Thanh")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
 // для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
 // COM, задайте атрибуту ComVisible значение TRUE для этого типа.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: Guid("3df078d0-a043-4e10-86da-40d19b11142b")]
@@ -33,6 +33,6 @@ using System.Resources;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.3.3.0")]
-[assembly: AssemblyFileVersion("3.3.3.0")]
-[assembly: NeutralResourcesLanguageAttribute("ru")]
+[assembly: AssemblyVersion("3.3.4.0")]
+[assembly: AssemblyFileVersion("3.3.4.0")]
+[assembly: NeutralResourcesLanguage("en")]
